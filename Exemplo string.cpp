3d99add1c3d9de4758d42d3e3184a7f93a7120bc
AnaCarolHub <anacarolline20099@gmail.com>
2015-05-24 00:30:09 -0300

    First Commit

---
+++ Exemplo string.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+main(){
+
+char texto[100], inverso [100];
+int i,f;
+printf ("Digite um texto: ");
+gets (texto);
+printf("O tamanho e: %d \n", strlen (texto));
+
+f=0;
+for (i=strlen(texto)-1 ; i>=0; i--) {
+	inverso[f] = texto[i];
+	f++;
+}
+
+	inverso[f] = '\0';
+	printf ("O inverso e : %s\n", inverso); 
+
+
+	printf("\n\n");
+	system ("pause");
+}
+
